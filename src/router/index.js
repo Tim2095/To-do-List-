8dd9be8178d368a44e18ab8cf05e5319b519029e
Timur Mosenkov <timur.mosenkov@gmail.com>
2023-03-15 09:34:23 +0200

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AddTask from '../components/AddTask.vue'
 import HomePage from '../views/HomePage.vue'
-import AllTasks from '../components/allTasks.vue'
+import AllTasks from '../components/AllTasks.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
