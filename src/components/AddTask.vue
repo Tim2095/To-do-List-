8bd491d464a409f6723e5e63055d98a43486d9a5
Timur Mosenkov <timur.mosenkov@gmail.com>
2023-03-15 09:27:00 +0200

    Add close tas on logo

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="add-task__container">
     <div class="add-task__header">
-      <h3>ADD TASK</h3>
-      <img src="../assets/img/Vector.png" alt="close-task" />
+      <h3>add task</h3>
+      <router-link to="/">
+        <img src="../assets/img/Vector.png" alt="close-task" />
+      </router-link>
     </div>
     <div class="add-task__logo">
       <img src="../assets/img/Logo.png" alt="logo" />
@@ -11,15 +13,15 @@
     <form>
       <div class="title-content">
         <label for="title">Title</label>
-        <input type="text" id="title" />
+        <input type="text" id="title" placeholder="Enter task title" />
       </div>
       <div class="description-content">
         <label for="description">Description</label>
-        <input type="text" id="description" />
+        <input type="text" id="description"  placeholder="Enter task description"/>
       </div>
       <div class="endingDate-content">
         <label for="endingDate">Date end</label>
-        <input type="date" id="endingDate" />
+        <input type="date" id="endingDate"/>
       </div>
       <div class="task-btn-actions">
         <task-button bgr="add">Add</task-button>
@@ -44,6 +46,14 @@ import TaskButton from '../ui/TaskButton.vue'
     display: flex;
     justify-content: space-between;
     align-items: center;
+
+    h3 {
+      color: #1AB8DB;
+      font-size: 1.6rem;
+      font-weight: 700;
+      font-family: 'Montserrat';
+      text-transform: uppercase;
+    }
   }
 
   .add-task__logo {
@@ -78,6 +88,7 @@ import TaskButton from '../ui/TaskButton.vue'
       border: 1px solid rgba(26, 184, 219, 0.85);
       border-radius: 10px;
       font-size: 1.6rem;
+      padding-left: 1rem;
     }
 
     input[type=date] {
