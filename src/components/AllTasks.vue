9f5941870a9c6b8fb4df21029b95e394386255fb
Timur Mosenkov <timur.mosenkov@gmail.com>
2023-03-16 21:33:55 +0200

    fix

--- src/components/AllTasks.vue
+++ src/components/AllTasks.vue
@@ -4,6 +4,10 @@
       <div class="all-task__logo">
         <img src="../assets/img/Logo.png" alt="logo" />
       </div>
+      <div class="tasks__ammount">
+        <h4>8 tasks</h4>
+        <h4>View all</h4>
+      </div>
     </header>
   </div>
 </template>
@@ -11,12 +15,15 @@
 <style lang="scss" scoped>
 .all-tasks__header {
   background: #222222;
+  color: #fff;
 
   .all-task__logo {
     display: flex;
     justify-content: center;
     align-items: center;
-    transform: translateY(50%);
+    margin-bottom: 0.8rem;
+    transform: translateY(50%); 
+    
   }
 }
 </style>
